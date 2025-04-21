800f4da43540aaba56b6b75e51748e4fcf97e888
NoNameAKA <130733726+NoName9989@users.noreply.github.com>
2025-04-21 22:27:11 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- init.lua
-local Core = loadstring(game:HttpGet("https://raw.githubusercontent.com/NoName9989/NNGui/main/core/main.lua"))()
+local Core = loadstring(game:HttpGet("https://raw.githubusercontent.com/NoName9989/NNGui/refs/heads/main/core/main.lua"))()
 return Core
